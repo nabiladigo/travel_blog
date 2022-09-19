9222f8622b1fd9833d5bebe1fb0f042d1f900245
nabiladigo <nabiladigo91@gmail.com>
2022-09-19 18:03:21 -0400

    adding home and about pages

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from pipes import Template
+from django.shortcuts import render 
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html"
+    
+
+class About(TemplateView):
+    template_name = "about.html"
+   
