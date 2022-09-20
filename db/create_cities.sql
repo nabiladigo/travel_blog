d751b6ba8d85a6f925db5dc25d8b70200c14db36
nabiladigo <nabiladigo91@gmail.com>
2022-09-19 20:30:05 -0400

    create cities data

--- db/create_cities.sql
+++ db/create_cities.sql
@@ -1,32 +1,30 @@
-INSERT INTO main_app_artist(name, img, country, created_at)
+INSERT INTO main_app_city(name, img, country,  created_at)
   VALUES
   ('OUARZAZATE', 'https://imgur.com/upJv4H2','MOROCCO',19-09-2022)
-
-INSERT INTO main_app_artist(name, img, country, created_at)
+INSERT INTO main_app_city(name, img, country, created_at)
+  VALUES 
+  ('CASABLANCA','https://imgur.com/zD21BJO','MOROCCO',19-09-2022)
+INSERT INTO main_app_city(name, img, country, created_at)
+  VALUES 
+  ('CHEFCHAOUEN','https://imgur.com/Ln9wg1q','MOROCCO',19-09-2022)
+INSERT INTO main_app_city(name, img, country, created_at)
+  VALUES 
+  ('TANGIER','https://imgur.com/WXvoFkh','MOROCCO',19-09-2022)
+INSERT INTO main_app_city(name, img, country, created_at)
+  VALUES 
+  ('MERZOUGA','https://imgur.com/vlJZHk3','MOROCCO',19-09-2022)
+INSERT INTO main_app_city(name, img, country, created_at)
+  VALUES 
+  ("M'HAMID",'https://imgur.com/nkCJTbV','MOROCCO',19-09-2022)
+INSERT INTO main_app_city(name, img, country, created_at)
+  VALUES 
+  ('AGADIR','https://imgur.com/mxNJMp2','MOROCCO',19-09-2022)
+INSERT INTO main_app_city(name, img, country, created_at)
+  VALUES 
+  ('ESSAOUIRA','https://imgur.com/kG21wdu','MOROCCO',19-09-2022)
+INSERT INTO main_app_city(name, img, country, created_at)
+  VALUES 
+  ('FES','https://imgur.com/6slyagb','MOROCCO',19-09-2022)
+INSERT INTO main_app_city(name, img, country, created_at)
   VALUES
-  'MOROCCO',19-09-2022)
-
-INSERT INTO main_app_artist(name, img, country, created_at)
-  VALUES 
-  'MOROCCO',19-09-2022)
-
-INSERT INTO main_app_artist(name, img, country, created_at)
-  VALUES 
-  'MOROCCO',19-09-2022)
-
-INSERT INTO main_app_artist(name, img, country, created_at)
-  VALUES 
-  'MOROCCO',19-09-2022)
-
-INSERT INTO main_app_artist(name, img, country, created_at)
-  VALUES 
-  'MOROCCO',19-09-2022)
-
-INSERT INTO main_app_artist(name, img, country, created_at)
-  VALUES 
-  'MOROCCO',19-09-2022)
-
-INSERT INTO main_app_artist(name, img, country, created_at)
-  VALUES 
-  'MOROCCO',19-09-2022)
- 
+  ('MARRAKESH','https://imgur.com/vW651Xw','MOROCCO',19-09-2022)
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.http import HttpResponse
+from .models import City
 
 
 # Create your views here.
@@ -16,7 +17,18 @@ class About(TemplateView):
 
 class Cities(TemplateView):
     template_name="city_list.html"
-    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name= self.request.GET.get("name")
+        if name != None:
+            context["cities"]= City.objects.filter(name__icontains=name)
+            context["header"]= f"Searching for {name}"
+        else:
+            context["cities"] = City.objects.all()
+            context["header"]= "Trending Cities"
+        return context
+
 class Post(TemplateView):
     template_name="post_list.html"
    
