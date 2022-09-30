651d95a9720bf81e657c53ae0dd7fd7a5f14ad1e
nabiladigo <nabiladigo91@gmail.com>
2022-09-29 23:28:01 -0400

    still working

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from . import views
-from .views import profile, postlike
+from .views import  profile, postlike
 
 urlpatterns =[
     path('', views.Home.as_view(), name="home"),
@@ -19,7 +19,7 @@ urlpatterns =[
     path('like/<int:pk>/', postlike, name="post_like"),
 
     path('accounts/signup', views.Signup.as_view(), name="signup"),
-    # path('profile/<int:pk>', views.ProfilePage.as_view(), name= "profile"),
+    # path('<int:pk>/profile/', views.ProfilePage.as_view(), name= "profile"),
     path('profile/', profile, name="profile"),
     path('profile_update/', views.ProfileUpdate.as_view(), name="profile_update"),
     path('password/', views.PasswordChangeView.as_view(), name="passsword_changed"),
